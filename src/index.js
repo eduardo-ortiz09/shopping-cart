3763cb1989525c49718fa185e8bf2cd99651bc30
Eduardo Ortiz <eduardo_ortiz09@hotmail.com>
2022-08-26 04:02:23 -0500

    Add App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Products from './Products';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Products />
   </React.StrictMode>
 );
 
